e1346f5225737700c87318bbd182ffff0350fff9
unknown <aresone.gaben@gmail.com>
2021-10-25 10:39:42 +0200

    match the letter

--- hangman.py
+++ hangman.py
@@ -5,20 +5,23 @@ def play(word, lives):
     wordArray = []
     wordPlaceHolderArray = []
     sumOfGuessedLetters = 0
-
     for i in word:
         wordArray.append(i)
         wordPlaceHolderArray.append("_")
 
     while sumOfGuessedLetters < len(wordArray):
+        i = 0
         guessedLetter = input("Guess a letter")
-        for i in word:
-            if(guessedLetter == i):
-                
-                print('You guessed it')
-                print(wordPlaceHolderArray)
+        for letter in wordArray:
+            i += 1
+            print(letter)
+            if(letter == guessedLetter):
+                print('Guessed it')
+                wordPlaceHolderArray[i - 1] = letter
+                sumOfGuessedLetters += 1
             else:
                 print('No match')
-        
+            print(wordPlaceHolderArray)   
+    
 
 play(word, 5)
