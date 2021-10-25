ec70e538d9bd3d56ed7a091167dbd45eb4dbfb2f
Marciusz66 <marciistvan45@outlook.hu>
2021-10-25 11:59:14 +0200

    added update

--- hangman.py
+++ hangman.py
@@ -1,7 +1,78 @@
 word = "marketplace"
 
+
 def play(word, lives):
-    
+    szakaszok = [
+            """
+                    --------
+                    |      |
+                    |      O
+                    |     \\|/
+                    |      |
+                    |     / \\
+                    -
+                    """,
+
+            """
+                    --------
+                    |      |
+                    |      O
+                    |     \\|/
+                    |      |
+                    |     / 
+                    -
+                    """,
+
+            """
+                    --------
+                    |      |
+                    |      O
+                    |     \\|/
+                    |      |
+                    |      
+                    -
+                    """,
+
+            """
+                    --------
+                    |      |
+                    |      O
+                    |     \\|
+                    |      |
+                    |     
+                    -
+                    """,
+
+            """
+                    --------
+                    |      |
+                    |      O
+                    |      |
+                    |      |
+                    |     
+                    -
+                    """,
+
+            """
+                    --------
+                    |      |
+                    |      O
+                    |    
+                    |      
+                    |     
+                    -
+                    """,
+
+            """
+                    --------
+                    |      |
+                    |      
+                    |    
+                    |      
+                    |     
+                    -
+                    """
+    ]
     wordArray = []
     wordPlaceHolderArray = []
     sumOfGuessedLetters = 0
@@ -10,18 +81,28 @@ def play(word, lives):
         wordPlaceHolderArray.append("_")
 
     while sumOfGuessedLetters < len(wordArray):
+
         i = 0
+        founds = 0
         guessedLetter = input("Guess a letter")
+        if(len(guessedLetter) > 1):
+            guessedLetter = input("bad formatted input, guess only one letter")
         for letter in wordArray:
             i += 1
-            print(letter)
             if(letter == guessedLetter):
-                print('Guessed it')
+                print('You got it')
                 wordPlaceHolderArray[i - 1] = letter
                 sumOfGuessedLetters += 1
-            else:
-                print('No match')
-            print(wordPlaceHolderArray)   
-    
+                founds += 1
+                print(wordPlaceHolderArray)
+        if (founds <= 0):
+            print('No match')
+            print(szakaszok[lives - 1])
+            lives -= 1
+        print('lives', lives)
+        if(lives <= 0):
+            print('You lost')
+            return
 
-play(word, 5)
+
+play(word, 7)
